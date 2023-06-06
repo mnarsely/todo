be2947a53ddae3a250434f20ee3fcf413014b7fc
Arsene <arsely8@gmail.com>
2023-06-06 13:26:07 +0200

    edit and delete functionality

--- todo/urls.py
+++ todo/urls.py
@@ -3,7 +3,14 @@ from . import views
 
 
 urlpatterns = [
+    # ADD Feature
     path('addTask/', views.addTask, name='addTask'),
+    # Mark as Done
     path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
-    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone')
+    # Mark as Undone
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+    # EDIT Feature
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    # DELETE Feature
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
 ]
